d310cc93aebd49c0f4f06dfcb480a0a797aa9d2c
eljakj <jadjakhlab@gmail.com>
2026-03-11 04:14:11 +0100

    Initials commit

--- index.js
+++ index.js
@@ -3,26 +3,33 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+
+
+localStorage.clear()
+let leadsFromLocalStorage =  JSON.parse(localStorage.getItem("myLeads"))
+
+console.log(leadsFromLocalStorage)
+
+
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads) )
     renderLeads()
+    
+    
 })
 
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        
-
         listItems += `
             <li>
-                <a target="_blank" href="${myLeads[i]}">
+                <a target='_blank' href='${myLeads[i]}'>
                     ${myLeads[i]}
                 </a>
             </li>
-            
         `
-
     }
     ulEl.innerHTML = listItems  
 }
